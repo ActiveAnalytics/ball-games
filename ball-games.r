4ee9b0daec8be56af51f045e34698578797b3114
Chibisi Chima-Okereke <chibisi@activeanalytics.co.uk>
2014-08-30 14:36:55 +0100

    Update ball-games.r

--- ball-games.r
+++ ball-games.r
@@ -1,5 +1,6 @@
 require(deSolve)
 
+# Function for the dynamic model
 ball <- function(t, y, parms)
 {
   with(as.list(c(parms, y)), {
@@ -9,27 +10,24 @@ ball <- function(t, y, parms)
     coeff <- (-.5/m)*rho*A*nv_W
     dy1 <- y[2]
     spin1 <- coeff*CL*(vw[2]*(y[6] - W[3]) - vw[3]*(y[4] - W[2]))/w
-    if(is.na(spin1))
+    if(is.na(spin1)) # prevents NaN
       spin1 <- 0
-    #dy2 <- coeff*(CD*(y[2] - W[1]) - CL*(vw[2]*(y[6] - W[3]) - vw[3]*(y[4] - W[2]))/w)
     dy2 <- coeff*CD*(y[2] - W[1]) - spin1
     dy3 <- y[4]
     spin2 <- CL*(vw[3]*(y[2] - W[1]) - vw[1]*(y[6] - W[3]))/w
     if(is.na(spin2))
       spin2 <- 0
-    #dy4 <- coeff*(CD*(y[4] - W[2]) - CL*(vw[3]*(y[2] - W[1]) - vw[1]*(y[6] - W[3]))/w)
     dy4 <- coeff*CD*(y[4] - W[2]) - spin2
     dy5 <- y[6]
     spin3 <- coeff*CL*(vw[1]*(y[4] - W[2]) - vw[2]*(y[2] - W[1]))/w
     if(is.na(spin3))
       spin3 <- 0
-    #dy6 <- coeff*(CD*(y[6] - W[3]) - CL*(vw[1]*(y[4] - W[2]) - vw[2]*(y[2] - W[1]))/w) - g
+    # no more accelation if ball hits the ground
     if(y[5] <= 0 & t != 0){
       dy6 <- coeff*CD*(y[6] - W[3]) - spin3
     }else{
       dy6 <- coeff*CD*(y[6] - W[3]) - spin3 - g
     }
-    #print(c(dy1, dy2, dy3))
     list(c(dy1, dy2, dy3, dy4, dy5, dy6))
   })
 }
@@ -45,7 +43,7 @@ CL = .319*(1 - exp(-2.48E-3 * w))
 pars <- c(rho = 1.22, w = w, vw = vw, W = c(0, 0, 0), m = 4.59E-2, CD = .45,
           A = pi*(4.27E-2/2)^2, g = 9.81, CL = CL)
 
-# To state that when the ball hits the ground motion stops
+# To state that when the ball hits the ground velocities go to zero
 rootfunc <- function(t, y, parms) y[5] # displacement in dir 3
 eventfunc <- function(t, y, parms) {
   y[5] <- y[5]
